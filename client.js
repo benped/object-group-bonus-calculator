3326a5a1e277dbcb17bb4ca6c029723c3b2d26c2
Josh Kralewski <joshkmanj@gmail.com>
2022-01-25 15:11:19 -0600

    Base project requirements completed

--- client.js
+++ client.js
@@ -44,17 +44,49 @@ console.log( employees );
 function bonusCalcFunction(employee) {
   let bonusPercentage=0;
   if (employee.reviewRating===5) {
-    bonusPercentage += .1;
+      bonusPercentage += .1;
   } else if (employee.reviewRating===4) {
-    bonusPercentage += .06;
+      bonusPercentage += .06;
   } else if (employee.reviewRating===3) {
-    bonusPercentage += .04;
+      bonusPercentage += .04;
   }//end review rating
-  if (employee.employeeNumber.length) {
-    console.log('This is true',employee.employeeNumber,employee.employeeNumber.length);
-    
-  }
+    if (employee.employeeNumber.length<=4) {
+      bonusPercentage += .05;
+  }//end seniority filter
+    if (Number(employee.annualSalary) > 65000 ) { 
+      bonusPercentage -= .01;
+    }//end high salary filter
+    if (bonusPercentage > .13){
+      bonusPercentage = .13;
+    }
+    if (bonusPercentage < 0) {
+      bonusPercentage = 0;
+    }//end high/low bonus filter
   return bonusPercentage
 }//end function
 
+
+function totalComp(array){
+  employeesBonus = [];
+  for ( let employee of array ) {
+  let bonusPercentage = bonusCalcFunction(employee);
+  let bonusPercentageString = ((bonusPercentage)*100)+'%';
+  let newComp = ( Number(employee.annualSalary) * bonusPercentage ) + Number(employee.annualSalary);
+  let bonus = Number(employee.annualSalary) * bonusPercentage;
+  let employeesObj = {
+    name: employee.name,
+    bonusPercentage: bonusPercentage,
+    totalComp: Math.round(newComp),
+    totalBonus: Math.round(bonus),
+  }//end create new object
+  console.log(`${employeesObj.name}: bonus percentage: ${bonusPercentageString}, new salary: ${employeesObj.totalComp}, total bonus: ${employeesObj.totalBonus}`);
+  employeesBonus.push(employeesObj);
+  }//end for/of loop
+  return employeesBonus;
+
+}
+
+
 console.log('basic test', bonusCalcFunction(employees[0]));
+console.log(totalComp(employees));
+
